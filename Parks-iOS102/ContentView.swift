771bb6f79eb448e222d48174460dc6384b50931d
Jon Toussaint <jondtoussaint@gmail.com>
2025-10-19 12:02:59 -0400

    Initial Commit

---
+++ Parks-iOS102/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parks-iOS102
+//
+//  Created by Jon Toussaint on 10/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parks-iOS102/Parks_iOS102App.swift
@@ -0,0 +1,17 @@
+//
+//  Parks_iOS102App.swift
+//  Parks-iOS102
+//
+//  Created by Jon Toussaint on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Parks_iOS102App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
